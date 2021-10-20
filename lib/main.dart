035d937776c7bdbe534fb811f7520bc77e758a70
Hao <haodngn@gmail.com>
2021-10-20 16:01:26 +0700

    add on boarding screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:it_support/screens/auth_screen/login_screen.dart';
+import 'package:it_support/screens/auth_screen/onboarding_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginScreen(),
+      home: OnboardingScreen(),
     );
   }
 }

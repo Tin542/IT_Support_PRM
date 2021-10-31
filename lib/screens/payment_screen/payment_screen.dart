303b8e18fdf71cb40e113a84c00ac82420f7c715
Hao <haodngn@gmail.com>
2021-10-31 21:57:18 +0700

    hot fix payment

--- lib/screens/payment_screen/payment_screen.dart
+++ lib/screens/payment_screen/payment_screen.dart
@@ -20,43 +20,40 @@ class PaymentScreen extends StatefulWidget {
 
 class _PaymentScreenState extends State<PaymentScreen> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    payment();
+  }
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Payment"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Pay"),
-          onPressed: () async {
-            print("price: " + widget.txtPrice);
-            var request = BraintreeDropInRequest(
-                tokenizationKey: 'sandbox_rzfmsfvm_gsnpfscyym7sj4b4',
-                collectDeviceData: true,
-                paypalRequest: BraintreePayPalRequest(
-                    amount: widget.txtPrice, displayName: 'haoDng'),
-                cardEnabled: true);
-            BraintreeDropInResult? result =
-                await BraintreeDropIn.start(request);
-            if (result != null) {
-              print(result.paymentMethodNonce.description);
-              print(result.paymentMethodNonce.nonce);
-              reqRef.child(widget.req!).update({'status': 'Hoàn thành'});
+    return Scaffold();
+  }
 
-              displayToastMessage("Thanh toán thành công", context);
+  Future<void> payment() async {
+    print("price: " + widget.txtPrice);
+    var request = BraintreeDropInRequest(
+        tokenizationKey: 'sandbox_rzfmsfvm_gsnpfscyym7sj4b4',
+        collectDeviceData: true,
+        paypalRequest: BraintreePayPalRequest(
+            amount: widget.txtPrice, displayName: 'haoDng'),
+        cardEnabled: true);
+    BraintreeDropInResult? result =
+        await BraintreeDropIn.start(request);
+    if (result != null) {
+      print(result.paymentMethodNonce.description);
+      print(result.paymentMethodNonce.nonce);
+      reqRef.child(widget.req!).update({'status': 'Hoàn thành'});
 
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => BottomNavBar()));
-            } else {
-              displayToastMessage("Thanh toán không thành công", context);
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => BottomNavBar()));
-            }
-          },
-        ),
-      ),
-    );
+      displayToastMessage("Thanh toán thành công", context);
+
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => BottomNavBar()));
+    } else {
+      displayToastMessage("Thanh toán không thành công", context);
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => BottomNavBar()));
+    }
   }
 }
 
